03c3b2dcef65323b9fc02a4d0380a15eca04b143
Install User <install@researchstudio.at>
2018-06-12 13:29:29 +0200

    einige kleine updates

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -3,7 +3,7 @@ import { QuestComponent } from '../quest/quest.component'
 import { QuestionSingle } from '../question-single';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-question',
@@ -12,48 +12,96 @@ import { Observable } from 'rxjs/Observable';
 })
 export class QuestionComponent implements OnInit {
 
-  @ViewChild('f') form: any;
+  @ViewChild('f') form:any;
   questions: Object[];
-  qList: QuestionSingle[];
+  qList: QuestionSingle[] = [];
   aList: String[] = [];
-  mapQuestAnswer: Map<string, string>;
+  mapQuestAnswer: Map<string,string>;
   correctAnswers: number = 0;
+  json1: string = `
+    {
+      "idq": "string1",
+      "question": "string",
+      "ida1": "ida1",
+      "a1": "string",
+      "ida2": "ida2",
+      "a2": "a2",
+      "ida3": "ida3",
+      "a3": "a3",
+      "idaCorrect": "idaCorrect"
+    }`;
 
-  constructor(private http: HttpClient) {
+  json: string = `[
+  {
+    "idq": "idq_5b111cc2-6e25-11e8-adc0-fa7ae01bbebc",
+    "question": "Wie heißt die Kampfkatze von He-Man?",
+    "ida1": "ida1_5b111f6a-6e25-11e8-adc0-fa7ae01bbebc",
+    "a1": "Bella",
+    "ida2": "ida2_5b1120b4-6e25-11e8-adc0-fa7ae01bbebc",
+    "a2": "Cringer",
+    "ida3": "ida3_5b1121e0-6e25-11e8-adc0-fa7ae01bbebc",
+    "a3": "Tiger",
+    "idaCorrect": "ida2_5b1120b4-6e25-11e8-adc0-fa7ae01bbebc"
+  },
+  {
+    "idq": "idq_5b112302-6e25-11e8-adc0-fa7ae01bbebc",
+    "question": "Wie heißt der Hauptgegner von He-Man?",
+    "ida1": "5b112410-6e25-11e8-adc0-fa7ae01bbebc",
+    "a1": "Wario",
+    "ida2": "5b1127b2-6e25-11e8-adc0-fa7ae01bbebc",
+    "a2": "Skeletor",
+    "ida3": "5b1128d4-6e25-11e8-adc0-fa7ae01bbebc",
+    "a3": "Shredder",
+    "idaCorrect": "5b1127b2-6e25-11e8-adc0-fa7ae01bbebc"
+  }]
+  `;
+
+  constructor(private http: HttpClient) {    
     console.log("Constructor of QuestionComponent.")
 
+    for(let str of this.json){
+      
+    }/*
     this.qList = [
-      new QuestionSingle("qid1", "Frage 1", "aid_1", "Select lists are used if you want to allow the user to pick from multiple options.Select lists are used if you want to allow the user to pick from multiple options.", "aid_44", "an 2", "aid_easd", "antwort 3", "aid_1"),
+      new QuestionSingle("qid1", "Frage 1", "aid_1", "Select lists are used if you want to allow the user to pick from multiple options.Select lists are used if you want to allow the user to pick from multiple options.", "aid_44", "an 2", "aid_easd", "antwort 3","aid_1"),
       new QuestionSingle("qid2", "Frage ??", "aid_2", "answer 1", "aid23asd", "an 2", "aid3", "antwort 3", "aid_2"),
-      new QuestionSingle("qid745", "Frage adfasd?", "aid_3", "answer 1",
-        "aid2_easd", "an 2", "aid3_easdf", "aid_3", "aid_3")
+      new QuestionSingle("qid745", "Frage adfasd?", "aid_3", "answer 1", 
+      "aid2_easd", "an 2", "aid3_easdf", "aid_3", "aid_3")
     ];
-
+    
     this.showCorrectAnswers();
-
-    for (let q of this.qList) {
-      console.log("cor:" + q.idaCorrect);
-    }
-
+   
+    for(let q of this.qList){
+      console.log("cor:"+q.idaCorrect );
+    } 
+   
     this.mapQuestAnswer = new Map();
     //set qid and correct answer id
-    this.qList.forEach(element => {
+    this.qList.forEach(element => {      
       this.mapQuestAnswer.set(element.idq, element.idaCorrect);
-    });
+    });*/
   }
   //https://api.github.com/users/seeschweiler
   ngOnInit() {
-
-
+    let arrU: QuestionSingle[] = [];
+    let u: QuestionSingle = new QuestionSingle();
+    let json1 = JSON.parse(this.json1);
+    Object.assign(u, json1);
+    let json = JSON.parse(this.json);
+    Object.assign(arrU, json);
+    console.log(u.question);
+    console.log(arrU.length);
+    this.qList.push(u);
+    this.qList = arrU;
     /*
     this.http.get('http://kohlmeise.cosy.sbg.ac.at/app/quizzes/').subscribe(data => {
       console.log(data);
     });*/
   }
-  showCorrectAnswers() {
+  showCorrectAnswers(){
     console.log("following correct answers:");
-    for (let i = 0; i < this.qList.length; i++) {
-      console.log("this.qList[i].idaCorrect: " + this.qList[i].idaCorrect);
+    for(let i=0; i<this.qList.length; i++){
+      console.log("this.qList[i].idaCorrect: "+this.qList[i].idaCorrect);      
     }
   }
 
@@ -66,56 +114,56 @@ export class QuestionComponent implements OnInit {
     console.log("clicked: " + chosen);
   }
 
-  workMap() {
+  workMap(){
     let map = new Map();
-    map.set("A", "B");
-    map.set("C", "d");
+    map.set("A","B");
+    map.set("C","d");
 
-    console.log(map.get("A") + " :" + map.size);
+    console.log(map.get("A")+" :"+ map.size);
     map.forEach((value: string, key: string) => {
       console.log(key, value);
-    });
+   });
 
     let jsonstr = this.mapToJson(map)
     console.log(jsonstr);
     let map4 = JSON.parse('{ "myString": "string", "myNumber": "4" }');
-    for (let ob in map4) {
+    for(let ob in map4){
       console.log(map4[ob]);
     }
-    console.log("obj:" + map4.size);
+    console.log("obj:"+ map4.size);
   }
 
-  showValues() {
+  showValues() {    
     console.log(this.form)
     let json = JSON.stringify(this.form.value);
-    console.log("json: " + json);
+    console.log("json: "+json);
     let map2 = JSON.parse(json);
     this.correctAnswers = this.calculatePoints(new Map(Object.entries(map2)));
-    for (let obj in map2) {
-      console.log("key: " + obj + ": " + map2[obj]);
-      for (let q of this.qList) {
-        console.log("cor:" + q.idaCorrect + ": " + map2[obj]);
-        if (q.idaCorrect == map2[obj]) {
+    for(let obj in map2){
+      console.log("key: "+obj+": "+map2[obj]);
+      for(let q of this.qList){
+        console.log("cor:"+q.idaCorrect +": "+map2[obj]);
+        if(q.idaCorrect==map2[obj]){
           console.log("correct answer!");
-          this.correctAnswers += 1;
+          this.correctAnswers+=1;
         }
-      }
+      }      
     }
-    console.log("CORRECTANSWERS: " + this.correctAnswers);
+    console.log("CORRECTANSWERS: "+this.correctAnswers);
     let map = new Map();
-    map.set("A", 1);
+    map.set("A",1);
     map.forEach((value: string, key: string) => {
       console.log(key, value);
-    });
-    console.log("size:" + map.size + ", " + this.form.value);
+  });
+    console.log("size:"+map.size+", "+this.form.value);
 
   }
 
-  mapToJson(map): string {
+  mapToJson(map):string {
     return JSON.stringify([map]);
   }
 
-  jsonToMap(jsonStr: string) {
+  jsonToMap(jsonStr:string) {
     return new Map(JSON.parse(jsonStr));
   }
   /**Calculates how many questions have been answered correctly.
@@ -123,39 +171,39 @@ export class QuestionComponent implements OnInit {
    * @param mapAnswers Map containing question ids and selected answer ids.
    * @returns number - The amount of correct answers.
    */
-  calculatePoints(mapAnswers: Map<string, string>): number {
+  calculatePoints(mapAnswers:Map<string, string>):number{
     let correctAnswers: number = 0;
-
-    for (let obj in mapAnswers) {
-      console.log(" mapAnswers[obj] given answer: " + mapAnswers[obj])
-      if (mapAnswers[obj] in this.mapQuestAnswer.values()) {
+    
+    for(let obj in mapAnswers){
+      console.log(" mapAnswers[obj] given answer: "+mapAnswers[obj])
+      if(mapAnswers[obj] in this.mapQuestAnswer.values()){
         correctAnswers++;
       }
     }
     this.showCorrectAnswers();
-    for (let obj in this.mapQuestAnswer) {
-      console.log("correct answer:this.mapQuestAnswer[obj]: " + this.mapQuestAnswer[obj]);
+    for(let obj in this.mapQuestAnswer){
+      console.log("correct answer:this.mapQuestAnswer[obj]: "+this.mapQuestAnswer[obj]);
     }
-
+    
     let map = new Map();
-    map.set("A", "B");
-    map.set("C", "d");
+    map.set("A","B");
+    map.set("C","d");
 
-    console.log(map.get("A") + " :" + map.size);
-    console.log("MAP JSON: " + JSON.stringify(map));
+    console.log(map.get("A")+" :"+ map.size);
+    console.log("MAP JSON: "+JSON.stringify(map)); 
     map.forEach((value: string, key: string) => {
       console.log(key, value);
-    });
-
-    for (let [key, value] of map) {
+   });
+   
+    for(let [key, value] of map){
       console.log("for-of map" + key, value);
     }
 
     mapAnswers.forEach((value: string, key: string) => {
-      console.log("mapAnswers: " + key, value);
-    });
+      console.log("mapAnswers: "+key, value);      
+  });
 
-    console.log("CORRECTANSWERS: " + correctAnswers);
+    console.log("CORRECTANSWERS: "+correctAnswers);
     return correctAnswers;
   }
 }
